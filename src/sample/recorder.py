afc2eaef9f300e8a3e03c3fb656b37b02a5f2a71
SMavr <smavrikis@gmail.com>
2023-09-03 10:44:50 +0300

    Did some renaming

---
+++ src/sample/recorder.py
@@ -0,0 +1,69 @@
+import pyaudio
+import wave
+import audioop
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+def record():
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    
+    print("Start recording")
+
+    frames = []
+
+    try:
+        while True:
+            data = stream.read(CHUNK)
+            print_volume(data)
+            frames.append(data)
+    except KeyboardInterrupt:
+        print("Done recording")
+    except Exception as e:
+        print(str(e))
+
+
+    sample_width = p.get_sample_size(FORMAT)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    
+    return sample_width, frames
+
+
+def record_to_file(file_path):
+    wf = wave.open(file_path, 'wb')
+    wf.setnchannels(CHANNELS)
+    sample_width, frames = record()
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def print_volume(data:bytes):
+    rms = audioop.rms(data, 2)
+    print(' ' * 20, end='\r')
+    print('#' * int((rms/500)), end='\r')
+    
+
+if __name__ == '__main__':
+    print('#' * 80)
+    print("Please speak inot the microphone")
+    print("Press Ctrl+C to stop recording")
+
+    record_to_file('output.wav')
+
+    
+    print("Result written to output.wav")
+    print('#' * 80)
--- src/test.py
+++ src/test.py
@@ -1,12 +1,12 @@
 # 1. Record for 1 min.
 # 2. Find note.
 # 3. Display note.
-import recorder_test
-import music_note_detector_test
+import recorder
+import music_note_detector
 
 if __name__ == "__main__":
-    sound = recorder_test.record()
-    note = music_note_detector_test.note_detect_2(sound)
+    sound = recorder.record()
+    note = music_note_detector.note_detect_2(sound)
     print("\n\tDetected Note = " + str(note))
     
 
