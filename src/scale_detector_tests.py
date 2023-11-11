dff725a751b4dd8e187f977d2f10cf2989521d28
SMavr <smavrikis@gmail.com>
2023-11-11 09:55:10 +0200

    added another test

--- src/scale_detector_tests.py
+++ src/scale_detector_tests.py
@@ -3,10 +3,9 @@ import numpy as np
 import pytest
 
 
-    # Define the test cases using the @pytest.mark.parametrize decorator
 @pytest.mark.parametrize("recognized_notes, is_fisiko_armoniko_scale", [
     (np.array(["E0","F0","G0"]), True),
-    # Add more test cases here as needed
+    (np.array(["E0", "F#0", "G0"]), False)
 ])
 def test_detect_scale(recognized_notes, is_fisiko_armoniko_scale):
     result = scale_detector.detect_scale(recognized_notes)
