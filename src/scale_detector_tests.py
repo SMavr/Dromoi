eea57b753e6955ef9485614e1acccac7dde09b8c
SMavr <smavrikis@gmail.com>
2023-11-11 09:23:46 +0200

    used pytest

--- src/scale_detector_tests.py
+++ src/scale_detector_tests.py
@@ -1,13 +1,11 @@
-import unittest
 import scale_detector
 import numpy as np
+import pytest
 
-class TestScaleDetect(unittest.TestCase):
+@pytest.mark.parametrize("recognized_notes",[
+    np.array(["F3", "C3"])
+])
   
-    def test_detect_scale(self):
-        recognized_notes = np.array(["F3", "C3"]);
-        result = scale_detector.detect_scale(recognized_notes)
-        self.assertTrue(result)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_detect_scale(recognized_notes):
+    result = scale_detector.detect_scale(recognized_notes)
+    assert result == True
