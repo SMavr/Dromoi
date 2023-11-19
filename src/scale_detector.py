8ce911ef90f517e077ab71b35826839e0ebdd792
SMavr <smavrikis@gmail.com>
2023-11-19 10:11:31 +0200

    can detect specific scale

--- src/scale_detector.py
+++ src/scale_detector.py
@@ -1,9 +1,18 @@
 fisiko_minore_d_set = set(["D","E", "F", "G", "A", "A#", "C", "D"])
+armoniko_minore_d_set = set(["D","E", "F", "G", "A", "A#", "C#", "D"])
 
+# Αρμονικό Μινόρε Re-
 def detect_scale(notes):
     for note in notes:
         note_without_octave = note[:-1]  # Remove the octave number from the note name
         if note_without_octave not in fisiko_minore_d_set:
             return False
 
+    return True
+
+
+def is_scale(scale, notes):
+    for note in notes:
+        if note not in scale:
+            return False
     return True
--- src/scale_detector_tests.py
+++ src/scale_detector_tests.py
@@ -4,10 +4,10 @@ import pytest
 
 
 @pytest.mark.parametrize("recognized_notes, is_fisiko_armoniko_scale", [
-    (np.array(["E0","F0","G0"]), True),
-    (np.array(["E0", "F#0", "G0"]), False)
+    (np.array(["E","F","G"]), True),
+    (np.array(["E", "F#", "G"]), False)
 ])
 
-def test_detect_scale(recognized_notes, is_fisiko_armoniko_scale):
-    result = scale_detector.detect_scale(recognized_notes)
+def test_is_scale(recognized_notes, is_fisiko_armoniko_scale):
+    result = scale_detector.is_scale(scale_detector.fisiko_minore_d_set, recognized_notes)
     assert result == is_fisiko_armoniko_scale
