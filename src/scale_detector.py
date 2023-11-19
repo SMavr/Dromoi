b79873e13b59f3dd41edcf31bba36c1481f23440
SMavr <smavrikis@gmail.com>
2023-11-19 11:16:20 +0200

    removed comment

--- src/scale_detector.py
+++ src/scale_detector.py
@@ -3,12 +3,12 @@ import numpy as np
 fisiko_minore_d_set = set(["D","E", "F", "G", "A", "A#", "C", "D"])
 armoniko_minore_d_set = set(["D","E", "F", "G", "A", "A#", "C#", "D"])
 
-# Αρμονικό Μινόρε Re-
 def detect_scale(notes):
     get_notes_without_octave = np.vectorize(lambda note: note[:-1])
     notes_without_octave = get_notes_without_octave(notes)
 
     output = ""
+
     if is_scale(fisiko_minore_d_set, notes_without_octave):
         output += " Fisiko Minore D"
 
