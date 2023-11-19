4f5f1d64faa206ca9894c72d9702d18bc4645dd3
SMavr <smavrikis@gmail.com>
2023-11-19 09:57:50 +0200

    Renamed scale

--- src/scale_detector.py
+++ src/scale_detector.py
@@ -1,9 +1,9 @@
-d_fisiko_armoniko_set = set(["D","E", "F", "G", "A", "A#", "C", "D"])
+fisiko_minore_d_set = set(["D","E", "F", "G", "A", "A#", "C", "D"])
 
 def detect_scale(notes):
     for note in notes:
         note_without_octave = note[:-1]  # Remove the octave number from the note name
-        if note_without_octave not in d_fisiko_armoniko_set:
+        if note_without_octave not in fisiko_minore_d_set:
             return False
 
     return True
