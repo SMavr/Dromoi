3c72a69c56ace96a058e0de63abe2df41eb78035
SMavr <smavrikis@gmail.com>
2023-11-19 10:32:34 +0200

    Added more tests

--- src/scale_detector.py
+++ src/scale_detector.py
@@ -3,12 +3,22 @@ armoniko_minore_d_set = set(["D","E", "F", "G", "A", "A#", "C#", "D"])
 
 # Αρμονικό Μινόρε Re-
 def detect_scale(notes):
-    for note in notes:
-        note_without_octave = note[:-1]  # Remove the octave number from the note name
-        if note_without_octave not in fisiko_minore_d_set:
-            return False
+    notes_without_octave = map(lambda note: note[:-1], notes)
 
-    return True
+    output = ""
+    if is_scale(fisiko_minore_d_set, notes_without_octave):
+        output += " Fisiko Minore D"
+    if is_scale(armoniko_minore_d_set, notes_without_octave):
+        output += " Armoniko Minore D"
+
+    return output    
+
+    # for note in notes:
+    #     note_without_octave = note[:-1]  # Remove the octave number from the note name
+    #     if note_without_octave not in fisiko_minore_d_set:
+    #         return False
+
+    # return True
 
 
 def is_scale(scale, notes):
--- src/scale_detector_tests.py
+++ src/scale_detector_tests.py
@@ -3,11 +3,23 @@ import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize("recognized_notes, is_fisiko_armoniko_scale", [
-    (np.array(["E","F","G"]), True),
-    (np.array(["E", "F#", "G"]), False)
+@pytest.mark.parametrize("recognized_notes, scale, is_fisiko_armoniko_scale", [
+    (np.array(["E","F","G"]), scale_detector.fisiko_minore_d_set, True),
+    (np.array(["E", "F#", "G"]), scale_detector.fisiko_minore_d_set, False),
+     (np.array(["E","F","G"]), scale_detector.armoniko_minore_d_set, True),
+    (np.array(["E", "F", "C"]), scale_detector.armoniko_minore_d_set, False)
 ])
 
-def test_is_scale(recognized_notes, is_fisiko_armoniko_scale):
-    result = scale_detector.is_scale(scale_detector.fisiko_minore_d_set, recognized_notes)
+def test_is_scale(recognized_notes, scale, is_fisiko_armoniko_scale):
+    result = scale_detector.is_scale(scale, recognized_notes)
     assert result == is_fisiko_armoniko_scale
+
+
+@pytest.mark.parametrize("notes, output", [
+    (np.array(["E4","F4","C#4"]), " Armoniko Minore D")
+
+])
+
+def test_detect_scale(notes, output):
+    result = scale_detector.detect_scale(notes)
+    assert result == output
