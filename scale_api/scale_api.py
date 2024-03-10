c5ce76ce254f43698cffefa979bbf18805573630
SMavr <smavrikis@gmail.com>
2024-03-10 10:31:16 +0200

    added some more scales

--- scale_api/scale_api.py
+++ scale_api/scale_api.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
+import supported_scales
 
 app = Flask(__name__)
-items = ['Apple', 'Banana', 'Orange', 'Mango', 'Pineapple']
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=items)
+    return render_template('index.html', items= supported_scales.collection)
 
 if __name__ == '__main__':
     app.run(debug=True)
